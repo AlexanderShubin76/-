f6e929604093a0f653a380b2c7e9faffd8f48da2
Alexander <pastukh2007@yandex.ru>
2022-10-17 21:56:23 +0300

    Task_4_new

--- Домашнее задание семинар 1/Task__4/Program.cs
+++ Домашнее задание семинар 1/Task__4/Program.cs
@@ -10,25 +10,34 @@ System.Console.Write("Введите число_3: ");
 double number_3 = double.Parse(Console.ReadLine()!);
 
 double max = number_1;
-if (max > number_2)
+// if (max > number_2)
+// {
+//     if (max > number_3)
+//     {
+//         Console.WriteLine($"Max = {max}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Max = {number_3}");
+//     }
+// }
+// else 
+// {
+//     if (number_2 > number_3)
+//     {
+//         Console.WriteLine($"Max = {number_2}");
+//     }
+//     else 
+//     {
+//         Console.WriteLine($"Max = {number_3}");
+//     }
+// }
+if (number_2 > max)
 {
-    if (max > number_3)
-    {
-        Console.WriteLine($"Max = {max}");
-    }
-    else
-    {
-        Console.WriteLine($"Max = {number_3}");
-    }
+    max = number_2;
 }
-else 
+if (number_3 > max)
 {
-    if (number_2 > number_3)
-    {
-        Console.WriteLine($"Max = {number_2}");
-    }
-    else 
-    {
-        Console.WriteLine($"Max = {number_3}");
-    }
+    max = number_3;
 }
+Console.WriteLine($"Max = {max}");
