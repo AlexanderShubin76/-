5e6bc5793031a75a06d548309dc0bee65a80f205
Alexander <pastukh2007@yandex.ru>
2022-10-21 21:08:13 +0300

    Task_10

---
+++ Homework_seminar_2/Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10:
+//  Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+int FindSecondNumber(int number)
+{
+    return ((number / 10) % 10);
+}
+System.Console.Write("Введите трехзначное число: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(FindSecondNumber(number_1));
