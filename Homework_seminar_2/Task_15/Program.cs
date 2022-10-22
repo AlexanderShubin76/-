5cb1cb55c4225052cedf22a5eb86d6cdd85b2e3e
AlexanderShubin76 <115036659+AlexanderShubin76@users.noreply.github.com>
2022-10-22 18:23:54 +0300

    Task_15_version_2

--- Homework_seminar_2/Task_15/Program.cs
+++ Homework_seminar_2/Task_15/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 15:
+// Задача 15:
 //  Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 //  и проверяет, является ли этот день выходным.
 // 6 -> да
@@ -6,13 +6,16 @@
 // 1 -> нет
 void ShowRestDay(int dayWeek) 
 {
-    if ((dayWeek == 6) || (dayWeek == 7)) 
+    if (dayWeek == 6 || dayWeek == 7) 
     System.Console.WriteLine("Да");
+
     if (dayWeek < 6)
     System.Console.WriteLine("Нет");
-    if ((dayWeek < 1) || (dayWeek > 7)) 
+
+    else if (dayWeek < 1 || dayWeek > 7) 
     System.Console.WriteLine("Некорректное значение");
 }
+
 System.Console.Write("Введите число: ");
 int dayNumber = int.Parse(Console.ReadLine()!);
 ShowRestDay(dayNumber);
