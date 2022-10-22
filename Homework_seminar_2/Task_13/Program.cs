938e69421b120319f6997da8ce121e209ba38c0e
AlexanderShubin76 <115036659+AlexanderShubin76@users.noreply.github.com>
2022-10-22 18:23:00 +0300

    Task_13_version_2

--- Homework_seminar_2/Task_13/Program.cs
+++ Homework_seminar_2/Task_13/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 13: 
+// Задача 13: 
 // Напишите программу, которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет.
 // 645 -> 5
@@ -9,16 +9,23 @@
 void FindThirdNumber(int number)
 {
     number = Math.Abs(number); 
+
     if (number < 100)
-System.Console.WriteLine("Третьей цифры нет");
-if (number>=100)
-{while (number >= 1000)
-{number = number / 10;}
-System.Console.WriteLine($"{number % 10}");
-}}
+    System.Console.WriteLine("Третьей цифры нет");
+
+    else
+    {
+        while (number >= 1000)
+        {
+            number = number / 10;
+        }
+        System.Console.WriteLine($"{number % 10}");
+    }
+}
+
 System.Console.Write("Введите число: ");
-int number_1 = int.Parse(Console.ReadLine()!);
-FindThirdNumber(number_1);
+int number1 = int.Parse(Console.ReadLine()!);
+FindThirdNumber(number1);
 
 // System.Console.Write("Введите число: ");
 // int number = Math.Abs(int.Parse(Console.ReadLine()!));
